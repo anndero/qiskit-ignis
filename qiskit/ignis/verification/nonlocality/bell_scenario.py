1c82aa720981d0f5890253a52eab4f6a9b429f9c
anndero <52163488+anndero@users.noreply.github.com>
2020-02-25 11:56:48 +0000

    Update bell_scenario.py

--- qiskit/ignis/verification/nonlocality/bell_scenario.py
+++ qiskit/ignis/verification/nonlocality/bell_scenario.py
@@ -7,8 +7,7 @@ import string
 
 
 class BellScenario():
-    """ General scenario of Bell experiment. 
-
+    """ General scenario of a Bell experiment. 
     Describes the general scenario of Bell experiment characterized by (n,s,d).
     Relates to optimization problems in testing Bell nonlocality:
     :class: 'qiskit.ignis.verification.nonlocality.bell_nonlocality'. 
@@ -18,7 +17,6 @@ class BellScenario():
         * mapping between index of constraint and inputs, outputs,
         * nonzero elements in local model of correlations,
         * verification of constraints' redundancy. 
-
     Attributes:
         n (int): the number of systems (i.e. number of qubits).
         s (list[int]): possible measurement settings per system.
@@ -33,15 +31,14 @@ class BellScenario():
         local_corr (dict): the dict mapping row indices to nonzero elements 
             in Bell's local model of correlations.
         nonredundant (dict): the dict mapping row indices to bool indicating 
-            if a given row is nonredundant (True) or redundant (False).
-            
+            if a given row is nonredundant (True) or redundant (False).   
     """
 
     def __init__(self, sett, d = 2):
-        """ Initializes BellScenario with attributes.
+        """ Initialize BellScenario with attributes.
         Args:
             sett: sequence that specifies the number of possible measurement 
-                settings per system; len(sett) specifies the number of systems.
+                settings per system; len(sett) specifies the number of subsystems.
             d: specifies the number of possible outcomes [Default: 2 (qubits)].
         """
         self._s = []
@@ -58,19 +55,19 @@ class BellScenario():
     # --------------------------------------------------------------------------
     @property
     def n(self) -> int:
-        """ Returns the number of systems (i.e. number of qubits). """
+        """ Return the number of subsystems (i.e. number of qubits)."""
         return len(self.s)
 
     @property
     def s(self) -> list:
-        """ Returns possible measurement settings per system. """
+        """ Return possible measurement settings per subsystem."""
         return self._s
 
     @s.setter
     def s(self, s):
         if isinstance(s, str):
             raise TypeError(
-                'String sequence can be ambiguous in multisetting scenarios.') 
+                "String sequence can be ambiguous in multisetting scenario.") 
         sett = [self._check_int(i,'setting') for i in s]
         if any(i <= 0 for i in sett):
             raise ValueError('{} should be int > 0'.format(i))
@@ -78,8 +75,8 @@ class BellScenario():
 
     @property
     def d(self) -> int:
-        """ Returns the number of possible outcomes, equal to 
-            local Hilbert space dimension and the same for each system. """
+        """ Return the number of possible outcomes, equal to the
+            local Hilbert space dimension and the same for each subsystem."""
         return self._d
 
     @d.setter
@@ -94,8 +91,8 @@ class BellScenario():
     # --------------------------------------------------------------------------
     @property
     def rowsA(self) -> int:
-        """ Returns the dimension of space of behaviours 
-            (i.e. number of All rows in LP problem). """
+        """ Return the dimension of the space of behaviours 
+            (i.e. number of All rows in LP problem)."""
         if self.s:
             return int(np.prod(self.s, dtype=np.float64)*(self.d**self.n)) 
         else:
@@ -103,13 +100,13 @@ class BellScenario():
 
     @property
     def rowsB(self) -> int:
-        """ Returns the dimension of Bell polytope 
+        """ Return the dimension of Bell polytope 
             (i.e. number of rows in reduced LP problem). """
         return int(np.prod(np.array(self.s)*(self.d-1)+1, dtype=np.float64) - 1)
 
     @property
     def cols(self) -> int:
-        """ Returns the length of joint probabilities in LHV model 
+        """ Return the length of joint probabilities in LHV model 
             (i.e. number of columns in LP problem). """
         return self.d**sum(self.s) if self.s else 0
 
@@ -117,7 +114,7 @@ class BellScenario():
     # row and inputs & output mapping methods
     # --------------------------------------------------------------------------
     def inputs(self, row: int) -> list:
-        """ Returns the list of measurements' indices choosen by all observers. 
+        """ Return the list of measurements' indices choosen by all observers. 
         Args:
             row (int): an index of a constraint in an optimization problem.
         Returns:
@@ -127,8 +124,8 @@ class BellScenario():
         """
         row = self._check_int(row,'row')
         if not (0 <= row < self.rowsA):
-            raise ValueError('Row index {0} out of range for this problem: '
-                             '0 <= row < {1}'.format(row, self.rowsA))
+            raise ValueError("Row index {0} out of range for this problem: "
+                             "0 <= row < {1}".format(row, self.rowsA))
         m = []
         mid = row//(self.d**self.n)
         spr = [np.prod(self.s[-i:]) for i in range(1, self.n)]
@@ -139,7 +136,7 @@ class BellScenario():
         return list(map(int, m))
 
     def outputs(self, row: int) -> list:
-        """ Returns the list of outcomes received by all observers. 
+        """ Return the list of outcomes received by all observers. 
         Args:
             row (int): an index of a constraint in an optimization problem.
         Returns:
@@ -149,8 +146,8 @@ class BellScenario():
         """
         row = self._check_int(row,'row')
         if not (0 <= row < self.rowsA):
-            raise ValueError('Row index {0} out of range for this problem: '
-                             '0 <= row < {1}'.format(row, self.rowsA))
+            raise ValueError("Row index {0} out of range for this problem: "
+                             "0 <= row < {1}".format(row, self.rowsA))
         r = []
         rid = row % (self.d**self.n)
         while rid > 0:
@@ -162,8 +159,8 @@ class BellScenario():
         return list(map(int, r))
 
     def row(self, inputs: list, outputs: list) -> int:
-        """ Returns an index of constraint (row) in an optimization problem, 
-            corresponding to choosen measurements (inputs) and results (outputs) 
+        """ Return an index of a constraint (row) in an optimization problem, 
+            corresponding to choosen measurements (inputs) and results (outputs). 
         Args:
             inputs (list[int]): list of indices of choosen measurements.
             outputs (list[int]): list of outcomes for each observer.
@@ -173,16 +170,16 @@ class BellScenario():
             ValueError: if either inputs or outputs are invalid.
         """
         if len(inputs) != self.n:
-            raise ValueError('len(inputs) doesn\'t match n')
+            raise ValueError("len(inputs) doesn't match n.")
         inputs = [self._check_int(i,'input') for i in inputs]
         if not all( 0 <= i < self.s[j] for j, i in enumerate(inputs)):
-            raise ValueError('some inputs out of range')
+            raise ValueError("Some inputs are out of range.")
         
         if len(outputs) != self.n:
-            raise ValueError('len(outputs) doesn\'t match n')
+            raise ValueError("len(outputs) doesn't match n.")
         outputs = [self._check_int(i,'output') for i in outputs]
         if not all( 0 <= i < self.d for i in outputs):
-            raise ValueError('some outputs out of range')
+            raise ValueError("Some outputs are out of range.")
         
         abc = string.digits+string.ascii_lowercase
         rid = int(''.join([abc[i] for i in outputs]), base=self.d)
@@ -195,22 +192,22 @@ class BellScenario():
     # --------------------------------------------------------------------------
     @property
     def local_corr(self) -> dict:
-        """ Returns the dictionary mapping row indices to lists of nonzero 
-            elements in Bell's local model of correlations. """
+        """ Return the dictionary mapping row indices to lists of nonzero 
+            elements in Bell's local model of correlations."""
         return self._local_corr
 
     @local_corr.setter
     def local_corr(self, corr: dict):
         if not isinstance(corr, dict):
-            raise TypeError('corr should be dict')
+            raise TypeError("corr should be dict.")
         if not all(isinstance(key,int) for key in corr):
-            raise ValueError('corr keys should be int')
+            raise ValueError("corr keys should be int.")
         if not all(isinstance(val,list) for val in corr.values()):
-            raise ValueError('corr values should be list')
+            raise ValueError("corr values should be list.")
         self._local_corr = corr
 
     def get_local_corr(self, row: int) -> list:
-        """ Returns list of indices of nonzero elements in Bell's local model 
+        """ Return a list of indices of nonzero elements in Bell's local model 
             of correlations for a given row. 
         Args:
             row (int): an index of a constraint in an optimization problem.
@@ -224,7 +221,7 @@ class BellScenario():
         return self._local_corr.setdefault(row, self._non0ind(row))
 
     def _non0ind(self, row: int) -> list:
-        """ Determines indices of nonzero elements in Bell's local model of 
+        """ Determine indices of nonzero elements in Bell's local model of 
             correlations for a given row. 
         Args:
             row (int): an index of a constraint in an optimization problem.
@@ -257,7 +254,7 @@ class BellScenario():
     # --------------------------------------------------------------------------
     @property
     def nonredundant(self) -> dict:
-        """ Returns a dict mapping row indices to bool indicating if a given row 
+        """ Return a dict mapping row indices to bool indicating if a given row 
             is nonredundant (True) or redundant (False) due to nosignaling rule. 
         """
         return self._nonredundant
@@ -265,15 +262,15 @@ class BellScenario():
     @nonredundant.setter
     def nonredundant(self, nonredundant: dict):
         if not isinstance(nonredundant, dict):
-            return TypeError('nonredundant should be dict')
+            return TypeError("nonredundant should be dict.")
         if not all(isinstance(key,int) for key in nonredundant):
-            raise ValueError('nonredundant keys should be int')
+            raise ValueError("nonredundant keys should be int.")
         if not all(isinstance(val,bool) for val in nonredundant.values()):
-            raise ValueError('nonredundant values should be bool')
+            raise ValueError("nonredundant values should be bool.")
         self._nonredundant = nonredundant
 
     def get_nonredundant(self, row: int) -> bool:
-        """ Returns a bool indicating if a given row is nonredundant (True) 
+        """ Return a bool indicating if a given row is nonredundant (True) 
             or redundant (False) due to nosignaling rule. 
         Args:
             row (int): an index of a constraint in an optimization problem.
@@ -288,21 +285,22 @@ class BellScenario():
         return self._nonredundant.setdefault(row, self.isnonredundant(row))
 
     def nonredundant_full_list(self) -> list:
-        """ Returns full list of nonredundant constraints. 
+        """ Return the full list of nonredundant constraints. 
         Returns:
             list[int]: indices of nonredundant constraints."""
         return [row for row in range(self.rowsA) if self.get_nonredundant(row)]
 
     def isnonredundant(self, row: int) -> bool:
-        """ Verifies redundancy of constraint in an optimization problem. 
+        """ Verify redundancy of a constraint in an optimization problem. 
         Args:
             row (int): an index of a constraint in an optimization problem.
         Returns:
             bool: True if row is nonredundant, False otherwise.
         Additional Information:
-            There's no unique way to choose nonredundant rows due to nosignaling 
-            Here: a row is redundant if any of the observers obtained a result 
-            equal to (d−1) while measuring an observable with index > 0.
+        -----------------------
+        There's no unique way to choose nonredundant rows due to nosignaling. 
+        Here: a row is redundant if any of the observers obtained a result 
+        equal to (d−1) while measuring an observable with index > 0.
         """ 
         if self._check_int(row,'row') == 0:
             return False
@@ -317,7 +315,7 @@ class BellScenario():
     # --------------------------------------------------------------------------
     @staticmethod 
     def _check_int(i, name='') -> int:
-        """ Validates integer input.
+        """ Validate integer input.
         Args:
             i (int or float or complex or str): variable expected to be int.
             name (str): name of variable in an error message [Default: ''].
@@ -328,15 +326,15 @@ class BellScenario():
         """ 
         if isinstance (i, int):
             if isinstance(i, (bool, np.bool_)):
-                raise ValueError(name +' can\'t be bool')
+                raise ValueError(name +" can't be bool")
             else:
                 return i
         if isinstance (i, complex):
             if i.imag == 0:
                 i = i.real
             else:
-                raise ValueError(name + ' should be real')
+                raise ValueError(name + " should be real")
         if not float(i).is_integer(): 
-            raise ValueError(name + ' should be int')    
+            raise ValueError(name + " should be int")    
         else:
-            return int(float(i))            
+            return int(float(i))     
