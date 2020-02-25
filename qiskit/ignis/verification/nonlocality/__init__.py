7fb978b4dcefe504d362c5c0fac1394edecee08a
anndero <52163488+anndero@users.noreply.github.com>
2020-02-25 11:55:51 +0000

    Update __init__.py

--- qiskit/ignis/verification/nonlocality/__init__.py
+++ qiskit/ignis/verification/nonlocality/__init__.py
@@ -1,3 +1,6 @@
-from qiskit.ignis.verification.nonlocality.circuits import state_circ, meas_circs, su2
+"""
+Bell nonlocality testing module.
+"""
+from qiskit.ignis.verification.nonlocality.circuits import BLocCircuits
 from qiskit.ignis.verification.nonlocality.bell_scenario import BellScenario
 from qiskit.ignis.verification.nonlocality.bell_nonlocality import BLocFitter
