2a1799d86a52f8a437bd93923b809cf40cad2f28
anndero <52163488+anndero@users.noreply.github.com>
2020-02-28 08:16:57 +0000

    Update __init__.py

--- qiskit/ignis/verification/nonlocality/__init__.py
+++ qiskit/ignis/verification/nonlocality/__init__.py
@@ -1,6 +1,18 @@
-"""
-Bell nonlocality testing module.
-"""
+# -*- coding: utf-8 -*-
+ 
+# This code is part of Qiskit.
+#
+# (C) Copyright IBM 2020.
+#
+# This code is licensed under the Apache License, Version 2.0. You may
+# obtain a copy of this license in the LICENSE.txt file in the root directory
+# of this source tree or at http://www.apache.org/licenses/LICENSE-2.0.
+#
+# Any modifications or derivative works of this code must retain this
+# copyright notice, and modified files need to carry a notice indicating
+# that they have been altered from the originals.
+
+"""Bell nonlocality testing module."""
 from qiskit.ignis.verification.nonlocality.circuits import BLocCircuits
 from qiskit.ignis.verification.nonlocality.bell_scenario import BellScenario
 from qiskit.ignis.verification.nonlocality.bell_nonlocality import BLocFitter
